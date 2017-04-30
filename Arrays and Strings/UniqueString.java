8b9824be40c3bf60466204ddcd5569178af72ad5
steventir <steven.tir@mail.mcgill.ca>
2017-04-29 20:32:37 -0400

    cleaned code

--- Arrays and Strings/UniqueString.java
+++ Arrays and Strings/UniqueString.java
@@ -6,7 +6,7 @@ class UniqueString {
 			System.out.println("Usage: UniqueString string");
 			System.exit(1);
 		}
-	    boolean res = isUniqueChars(args[0]);
+		boolean res = isUniqueChars(args[0]);
 		System.out.println(res);
 	}
 
@@ -14,19 +14,17 @@ class UniqueString {
 	// a-z -> can use a bit vector, ASCII :  128 characters, need boolean[]
 	//Suppose we are only given a-z characters
 	public static boolean isUniqueChars(String str) {
-        //int has 32 bits, which is suitable for 26 letters
-	    int checker = 0;
+		//int has 32 bits, which is suitable for 26 letters
+		int checker = 0;
 
-        for (int i = 0; i < str.length(); i++) {
+		for (int i = 0; i < str.length(); i++) {
 			int val = str.charAt(i) - 'a';
 
-			//check
+			//check if already used
 			if ( (checker & (1 << val)) > 0 )
 				return false;
-			else {
+			else 
 				checker |= (1 << val);
-			}
-
 		}
 
 		return true;
