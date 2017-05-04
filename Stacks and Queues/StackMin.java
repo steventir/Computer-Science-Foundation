ff65814b7702f32dc37078786ae1ffe03d87b519
steventir <steven.tir@mail.mcgill.ca>
2017-05-03 20:57:51 -0400

    fixed bug in push

--- Stacks and Queues/StackMin.java
+++ Stacks and Queues/StackMin.java
@@ -12,7 +12,8 @@ public class StackMin {
 		if (!min.isEmpty()) {
 			if (min.peek() > d)
 				min.push(d);
-		}	
+		} else if(min.head == null)
+			min.push(d);
 		data.push(d);
 	}
 
@@ -42,5 +43,34 @@ public class StackMin {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		StackMin sm = new StackMin();
+
+		sm.push(4);
+		sm.push(1);
+		sm.push(2);
+		sm.push(6);
+
+		System.out.println(sm.data);
+		System.out.println(sm.min);
+
+		sm.pop();
+		System.out.println(sm.data);
+		System.out.println(sm.min);
+
+		sm.pop();
+		System.out.println(sm.data);
+		System.out.println(sm.min);
+
+		sm.pop();
+		System.out.println(sm.data);
+		System.out.println(sm.min);
+		sm.pop();
+		System.out.println(sm.data);
+		System.out.println(sm.min);
+		sm.pop();
+		System.out.println(sm.data);
+		System.out.println(sm.min);
+		System.out.println(sm.isEmpty());
 	}
 }
